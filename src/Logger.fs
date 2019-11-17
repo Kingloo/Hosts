c1baa20c8412725dac84ac5ee00960b74270c287
Kingloo <k1ngloo@hotmail.com>
2019-11-17 18:58:56 +0100

    Minor Formatting

--- src/Logger.fs
+++ src/Logger.fs
@@ -5,12 +5,6 @@ module Logger =
     open System
     open System.IO
 
-    let printLine (writer: TextWriter) (line: string) =
-        writer.WriteLine(line)
-
-    let printLines (writer: TextWriter) (lines: seq<string>) =
-        lines
-            |> Seq.iter (printLine writer)
-
-    let printError (line: string) =
-        printLine Console.Error line
+    let printLine (writer: TextWriter) (line: string) = writer.WriteLine line
+    let printLines (writer: TextWriter) (lines: seq<string>) = lines |> Seq.iter (printLine writer)
+    let printError (line: string) = printLine Console.Error line
